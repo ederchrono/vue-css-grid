aa1da46c687c7b6a9179274307160de57a6bb099
Eder Diaz <ederchrono@gmail.com>
2017-09-06 11:17:12 -0500

    Export the components

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import grid from './components/grid.vue'
+import gridItem from './components/gridItem.vue'
+import responsive from './components/responsive.vue'
+
+export { grid, gridItem, responsive }
