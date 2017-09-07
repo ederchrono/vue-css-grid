9057080a521b66eca6e02d28f786f73fdef21866
Eder Diaz <ederchrono@gmail.com>
2017-09-07 09:55:31 -0500

    Add a non abstract responsive component (to avoid deprecation)

---
+++ src/components/na-responsive.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="false"></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      width: 0,
+      height: 0
+    }
+  },
+
+  methods: {
+    onResize () {
+      if (this.width !== window.innerWidth) {
+        this.width = window.innerWidth
+        this.$emit('widthChange', this.width)
+      }
+
+      if (this.height !== window.innerHeight) {
+        this.height = window.innerHeight
+        this.$emit('heightChange', this.height)
+      }
+    }
+  },
+
+  mounted () {
+    window.addEventListener('resize', this.onResize)
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+
+    this.$emit('widthChange', this.width)
+    this.$emit('heightChange', this.height)
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onResize)
+  }
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import grid from './components/grid.vue'
 import gridItem from './components/gridItem.vue'
 import responsive from './components/responsive.vue'
+import NonAbstractResponsive from './components/na-responsive.vue'
 
-export { grid, gridItem, responsive }
+export { grid, gridItem, responsive, NonAbstractResponsive }
