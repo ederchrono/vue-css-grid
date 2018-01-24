599dc7acf7ae8e8514dbccfef076a8946df1eda1
Eder Diaz <ederchrono@gmail.com>
2018-01-23 23:32:53 -0600

    change names of components

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import CssGrid from './components/CssGrid.vue'
 import CssGridItem from './components/CssGridItem.vue'
 import ViewportSize from './components/ViewportSize.vue'
 
-export { CssGrid, GridItem, ViewportSize }
+export { CssGrid, CssGridItem, ViewportSize }
