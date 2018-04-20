6e7d08abcc74db3c3abd715ff13717addfe9a633
Eder Diaz <ederchrono@gmail.com>
2018-04-19 23:36:36 -0500

    ✨ WIP add vuepress

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -7,10 +7,13 @@ const sourceMapEnabled = isProduction
   : config.dev.cssSourceMap
 
 module.exports = {
-  loaders: utils.cssLoaders({
-    sourceMap: sourceMapEnabled,
-    extract: isProduction
-  }),
+  loaders: {
+    ...utils.cssLoaders({
+      sourceMap: sourceMapEnabled,
+      extract: isProduction
+    }),
+    js: "babel-loader"
+  },
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
   transformToRequire: {
